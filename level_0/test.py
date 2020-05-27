2eb7b0e243d9ae3aedb5b605fdcaa92bdaea1202
“GunterPearson” <1715@holbertonschool.com>
2020-05-27 13:22:13 -0500

    pushing dir level_1

--- level_0/test.py
+++ level_0/test.py
@@ -1,3 +1,10 @@
 #!/usr/bin/python3
 
-print("hello there")
+from lxml import html
+import requests
+
+page = requests.get("http://158.69.76.135/level0.php")
+pload = {'id': '1715', 'holdthedoor':'submit'}
+for x in range(1024):
+    r = requests.post("http://158.69.76.135/level0.php", data = pload)
+print("done!")
